f0e32c91bbb473b784751d77fbc663bbc778f25d
Javier Quiroga <contact@javierquiroga.com>
2024-11-19 09:56:11 +0000

    Add canvas setup

---
+++ script.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector('#canvas-container')
+
+const CANVAS_DIMENSIONS = 16;
+const TOTAL_CELLS = CANVAS_DIMENSIONS ** 2;
+
+for (let i = 1; i <= TOTAL_CELLS; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell')
+    canvas.appendChild(cell)
+}
